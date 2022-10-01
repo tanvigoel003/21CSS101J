42d2b5dd9edfdd2314834c862256410f5ece591d
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-01 11:59:43 +0530

    Create 6.program to switch case with break.c

---
+++ 6.program to switch case with break.c
@@ -0,0 +1,31 @@
+#include<studio.h>
+int main()
+{
+  printf("\n\n\t\tStudytonight-Best place to learn \n\n\n");
+  //Local Variable Definition 
+  char grade;
+  printf('Enter your grade :\n");
+         scanf("%c", &grade);
+         switch(grade)
+         {
+           case 'A':
+           printf("Excellent\n");
+           break;
+           case 'B':
+           printf("keep it up!\n\n");
+           break;
+           case'c':
+           printf("well done \nbreak keyword takes execution to exit the switch case\n\n");
+           break;
+           case 'D':
+           printf("you passed\n");
+           break;
+           case'F':
+           printf('Better luck next time\n");
+                  break;
+                  default:
+                  printf("Invalid grade\n");
+                  }
+                  printf("Your grade is %c\n" , grade);
+                  return 0;
+                  }
