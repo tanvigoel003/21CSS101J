2987d7ee43ead119396b49ce9e8bf87bfe32639c
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-02 18:10:08 +0530

    Create 18.converting miles to kilometer.c

---
+++ 18.converting miles to kilometer.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  float miles , kilometers;
+  printf("enter fistance in miles=69");
+  scanf("%f" ,&miles);
+  kilometers = miles * 1.609;
+  print("%.2f Miles are equal to %.2f kilometer" , miles ,kilometers);
+        return 0;
+}
