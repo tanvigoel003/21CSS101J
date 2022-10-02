ba5123be422bfe8de49dea07ba463c42e52788e4
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-02 16:20:27 +0530

    Create 17.loop in c.c

---
+++ 17.loop in c.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  /*
+  always declare the variable before using them 
+  */
+  int i =0;  //declaration and initialisationat the same time
+  for(i = 0; i < 10; i++)
+  {
+    printf("i=%d\n",i);
+    /* consequently , when i equals 10 , the loop breaks. i is updated before the condition is checked- hence the value of i after exiting the loop is 10 
+    */
+  }
+  printf("\n\The value of i after exiting the loop is %d\n\n" , i);
+  printf("\nRemember that the loop condition checks the conditional statement before it loops again.\n\n");
+  printf("Consequently , when i equal 10 , the loop breaks .\n\n");
+  printf("i is updated before the condition is checked - hence the value of i after exiting the loop is 10 .\n\n");
+  return 0;
+}
