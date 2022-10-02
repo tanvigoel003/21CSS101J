74c7e3e3603c09d4c47519c7b352f071e48e51fb
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-02 16:05:33 +0530

    Create 16.finding largest and smallest value.c

---
+++ 16.finding largest and smallest value.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int a,b;
+int main()
+{
+  printf("\n\nEnter the two values to find the greatest and smallest number : 010 ,007\n");
+  scanf"("%d%d",&a , &b);
+    if(a == b)
+      printf("Both are equal\n");
+  else if (a < b)
+  {
+    printf("\n\nThe largest number is %03d\n" , b);
+    printf("\nThe smallest number is %03d\n" , a);
+    printf("\nThe largest number is %03d\n" , b);
+  }
+  else //only possibility remaining 
+  {
+    printf("the largest number is %03d\n", a);
+    printf("The smallest number is %03d\n" , b);
+  }
+  return 0;
+}
