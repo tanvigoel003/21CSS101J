e8a176df5bfa85568d1c2fa823045c7ab2324c13
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-02 12:00:19 +0530

    Create 13.adding digits of an integer.c

---
+++ 13.adding digits of an integer.c
@@ -0,0 +1,17 @@
+/* this program reads a positive integer which is 5 digit long and sums the digit in it */
+#include < studio.h>
+main() 
+{
+int digit_1 , digit_2 , digit_3 , digit_4, digit_5 , sum , number , n ; 
+scanf( "%d", &number) ; 
+n = number ; 
+digit_1 = n % 10 ; 
+n =n/10 
+digit_2 = n % 10 ; 
+n = n/10 ; 
+digit_4 = n %10 ; 
+n = n/10 ; 
+digit_5 = n; 
+sum = digit_1 + digit_2 + digit_3 + digit_4 + digit_5; 
+printf("sum of digits = %d\n" , sum ) ; 
+} /* end of main */
