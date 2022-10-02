8802e25bf204de07f5524cf658b90d0305d7c424
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-02 12:05:46 +0530

    Create 14.calculation of si.c

---
+++ 14.calculation of si.c
@@ -0,0 +1,9 @@
+/* Calculation of simple intrest */
+main()
+{
+  int p, n ;
+  float r , si ; p= 1000;n = 3 ; r = 8.5 ; 
+  /* formula for simple intrest */
+  si = p*n*r/100;
+  printf ( " %f" , si ); 
+}
