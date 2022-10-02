8fe70abb3997b2c608b08081cba553ae8cfa8d31
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-02 11:43:25 +0530

    Create 10.program to reverse the case of input charecter.c

---
+++ 10.program to reverse the case of input charecter.c
@@ -0,0 +1,16 @@
+#include<stdio,h>
+#include<ctype.h> //to use system defined function islowerand toupper
+int main()
+{
+  char alphabet;
+  printf("Enter an alphabet : t");
+  putchar('\n'); //to move to next line 
+  alphabet=getchar();
+  printf("\n\nReverse case of %c is T",alphabet);
+  if(islower(alphabet));
+  putchar(toupper(alphabet));
+  else
+    // must be an uppercase charecter
+    printf("%c",tolower(alphabet));
+  return 0;
+}
