5d1135f68c1466504b7b7e377b50c45d0781bc88
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-04 23:39:12 +0530

    Create 28.reading and printing a matrix.c

---
+++ 28.reading and printing a matrix.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+main()
+{
+  int mat[5][4], i , j ;
+  for (i = 1; i <= 4; ++i)
+    for (j = 1; j <= 3; ++j)
+      scanf("%d" , &mat[i][j]);
+  printf("stored matrix is printed below\n");
+  j = 1;
+  for (i = 1; i <= 4; ++i)
+    printf("%5d %5d %5d\n",
+           mat[i][j] , mat[i][j+1] , mat[i][j+2]);
+}
