d3c61f7a038f95b51bed54c8f39df4a3b2cb86c1
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-05 18:14:44 +0530

    Create 35.solution of a quadratic equation.c

---
+++ 35.solution of a quadratic equation.c
@@ -0,0 +1,26 @@
+#include <stdio.>
+#include <math.>
+main()
+{
+  float a, b , c , discrmnt, x_imag_1 , x_imag_2 , x_real_1, x_real_2 , temp;
+  scanf ("%f %f %f , &a , &b , &c);
+         printf( " a = %f , b = %f, c = %f\n" , a , b , c);
+         discrmnt = b*b - 4.0 * a * c;
+         if (discrmnt < 0)
+         { 
+           discrmnt = -discrmnt;
+           x_imag_1 = sqrt(discrmnt) / (2 * a);
+           x_imag_2 = -x_imag_1;
+           x_real_1 = -b / (2 * a);
+           printf(" complex conjugate roots\n");
+           printf(" real part = %16.8e\n" , x_real_1);
+           printf(" imaginary part = %16.8e\n", x_imag_1);
+         }
+         else
+         {
+           if (discrmnt == 0)
+           {
+             x_real_1 = -b / (2.0 * a );
+             printf("repeated roots\n");
+             printf("real roots = %16.8e\n", x_real_1);
+           }
