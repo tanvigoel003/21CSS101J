b64d6051036ac5c967e0952fa0a13eae736f6184
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-05 17:46:38 +0530

    Create 34.alternate method  to pick largest number.c

---
+++ 34.alternate method  to pick largest number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+main()
+{
+  int a, b, c, big ;
+  scanf("%d %d %d , &a, &b, &c);
+        printf("a = %4d, b = %4d, c = %4d\n" , a, b, c);
+        big = a;
+        if(b > big)
+        big = b 
+        if (c > big)
+        big = c ;
+        printf("the largest number is %4d\n", big);
+        }
