c257dc33d616a457ec956be6494de1e1f113c61c
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-05 17:38:15 +0530

    Create 33. picking largest number.c

---
+++ 33. picking largest number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+main()
+{
+  int a , b , c;
+  scanf("%d %d %d", &a , &b ,&c);
+  printf("a = %4d\n" , a);
+  else 
+    printf("c = %4d\n", c);
+}
+else 
+{
+  if (b > c)
+    printf("b = %4d\n", b);
+  else
+    printf("c = %4d\n", c);
+}
+}
