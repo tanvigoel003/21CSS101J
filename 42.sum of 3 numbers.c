17d2529715d6edf6f1a095b5568ed9bec9fc025b
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-09 21:28:57 +0530

    Create 42.sum of 3 numbers.c

---
+++ 42.sum of 3 numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int num1,num2,num3;         /* declaration of three variables */  
+int sum;
+char line_text[50];      /* line of input from keyboard */ 
+  
+int main() 
+{  
+    printf("Input three numbers separated by comma : 5, 10, 15"); 
+	fgets(line_text, sizeof(line_text), stdin);
+	sscanf(line_text, "%d, %d, %d", &num1, &num2, &num3);
+    sum = num1+num2+num3; 
+    printf("The sum of three numbers : %d\n", sum);  
+    return(0);  
+}  
