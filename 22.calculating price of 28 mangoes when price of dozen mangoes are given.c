625ae93c1fc4272addb47121e8c1a3e65f7553ed
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-12 10:12:21 +0530

    Update 22.calculating price of 28 mangoes when price of dozen mangoes are given.c

--- 22.calculating price of 28 mangoes when price of dozen mangoes are given.c
+++ 22.calculating price of 28 mangoes when price of dozen mangoes are given.c
@@ -2,15 +2,16 @@
 #include <stdio.h>
 main()
 { 
+  printf("Registration number = RA22110420110033");
 int rupees , paise , cost_of_28;
 float  cost_dozen, rcost_28; 
 /*read cost of 12 mangoes */
 scanf("%f" , &cost_dozen);
 printf("cost of dozen = %f\n", cost_dozen);
-rcost_28 = ( ( cost_dozen * 100.0/120.0) * 28.0 + 0.5 ) ; 
+rcost_28 = ( ( cost_dozen * 100.0/120.0) * 28.0 + 0.5 ); 
 cost_of_28 = rcost_28; 
 rupees = cost_of_28/100;
 paise = cost_of_28 % 100;
-printf (" cost 28 mangoes \n") ; 
-printf ( " Rs.%d ps.%d\n" , rupees , paise ) ; 
+printf (" cost 28 mangoes \n"); 
+printf ( " Rs.%d ps.%d\n" , rupees , paise ); 
 } /* End of main */
