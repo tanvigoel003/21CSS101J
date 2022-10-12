bae3340095631890dee7b3d768e4fab5a1b31991
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-12 10:02:43 +0530

    Update 18.converting miles to kilometer.c

--- 18.converting miles to kilometer.c
+++ 18.converting miles to kilometer.c
@@ -1,8 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
+  printf("Registration number = RA22110420110033");
   float miles , kilometers;
-  printf("enter fistance in miles=69");
+  printf("enter distance in miles = 69");
   scanf("%f" ,&miles);
   kilometers = miles * 1.609;
   print("%.2f Miles are equal to %.2f kilometer" , miles ,kilometers);
