c305d189e84b0b6eb4031fb971b60c5002826195
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-12 10:10:20 +0530

    Update 21.adding digits of an integ.c

--- 21.adding digits of an integ.c
+++ 21.adding digits of an integ.c
@@ -2,15 +2,16 @@
 #include < studio.h>
 main() 
 {
-int digit_1 , digit_2 , digit_3 , digit_4, digit_5 , sum , number , n ; 
-scanf( "%d", &number) ; 
-n = number ; 
-digit_1 = n % 10 ; 
-n =n/10 
-digit_2 = n % 10 ; 
-n = n/10 ; 
-digit_4 = n %10 ; 
-n = n/10 ; 
+  printf("Registration number = RA2211042010033");
+int digit_1 , digit_2 , digit_3 , digit_4, digit_5 , sum , number , n; 
+scanf( "%d", &number); 
+n = number; 
+digit_1 = n % 10; 
+n = n / 10; 
+digit_2 = n % 10; 
+n = n / 10; 
+digit_4 = n %10; 
+n = n / 10; 
 digit_5 = n; 
 sum = digit_1 + digit_2 + digit_3 + digit_4 + digit_5; 
 printf("sum of digits = %d\n" , sum ) ; 
