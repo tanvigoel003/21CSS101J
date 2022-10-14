03c2ae0dd492e0643bd933fd7c0f8149b5d26074
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-14 13:44:10 +0530

    Update 14.calculation of si.c

--- 14.calculation of si.c
+++ 14.calculation of si.c
@@ -1,7 +1,7 @@
 /* Calculation of simple intrest */
 int main()
 {
-  printf("Registration number = RA22110420110033"):
+  printf("Registration number = RA22110420110033"\n):
   int p, n ;
   float r , si ; p= 1000;n = 3 ; r = 8.5 ; 
   /* formula for simple intrest */
