bff284a235312a260e34970704266c23f503a513
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-14 13:42:08 +0530

    Update 12.swapping 2 numbers using addition and subtraction.c

--- 12.swapping 2 numbers using addition and subtraction.c
+++ 12.swapping 2 numbers using addition and subtraction.c
@@ -2,7 +2,7 @@
 #include<conio.h>
 void main()
 {
-  printf("Registration number = RA2211042010033");
+  printf("Registration number = RA2211042010033"\n);
   int x = 10 , y = 15;
   x = x + y - (y = x);
   printf("x = %d and y = %d" , x,y);
