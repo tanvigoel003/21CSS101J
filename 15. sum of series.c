be592cd1c371b312ca1add56a847550342321596
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-14 13:45:05 +0530

    Update 15. sum of series.c

--- 15. sum of series.c
+++ 15. sum of series.c
@@ -2,7 +2,7 @@
 #include <studio.h>
 int main()
 {
-  printf("registration number = RA2211042010033");
+  printf("registration number = RA2211042010033"\n);
   float x , x2 , x3 , x4 , x6 , x8 , x10 , sum ;
   scanf("%f" , &x );
   printf("x =%f \n" , x) ; x2 = x*x ;x4 = x2*x2 / 24.0 ; x6 = x4*x2 / 30.0 ;x8 = x6*x2 / 56.0 ;x10 = x8*x2 / 90.0 ;
