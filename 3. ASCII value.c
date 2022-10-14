aa17c01d5304ae33512b433ef84d9a43500af7af
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-14 14:17:47 +0530

    Update 3. ASCII value.c

--- 3. ASCII value.c
+++ 3. ASCII value.c
@@ -1,12 +1,10 @@
 #include<studio.h>
 int main()
 { 
-   printf("\n\n\tStudytonight - Best place to learn \n\n\n");
+   printf("registration number = RA2211042010033"\n);
    char c;
    printf("Entr a charecter : ");
    scanf("%c" , &c);
    printf("\n\nASCII value of %c = %d" ,c,c);
-   
-   printf("\n\n\t\t\t Coding is Fun !\n\n\n");
    return 0;
    }
