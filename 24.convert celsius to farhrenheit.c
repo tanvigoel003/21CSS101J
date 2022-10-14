91aaacdbb8ffe1ea7cba444b72074c393e14d558
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-14 14:02:52 +0530

    Update 24.convert celsius to farhrenheit.c

--- 24.convert celsius to farhrenheit.c
+++ 24.convert celsius to farhrenheit.c
@@ -1,7 +1,7 @@
 #include < studio.h>
 main()
 {
-  printf("registration number = RA22110420110033");
+  printf("registration number = RA22110420110033"\n);
 float fahrenheit , celsius; 
 scanf("%f" , &celsius);
 fahrenheit= 1.8*celsius + 32.0;
