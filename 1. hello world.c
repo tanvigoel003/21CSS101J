eafb731200441037eb559756df99a8e62e3db342
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-14 13:27:03 +0530

    Update 1. hello world.c

--- 1. hello world.c
+++ 1. hello world.c
@@ -1,6 +1,7 @@
 # include < studio.h>  //including header file in our program 
 int main()       // main() where the execution begins 
 {
+  printf("registration number = RA112211042010033",\n");
 printf("Hello World");
 return 0;
 }
