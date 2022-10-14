c32ae9f8fb03daa983abab6b544661c1bbdf2b05
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-14 14:23:13 +0530

    Update 31.area and perimeter of circle.c

--- 31.area and perimeter of circle.c
+++ 31.area and perimeter of circle.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main() 
 {
+  printf("registration number = RA2211042010033"\n);
   float radius , area;
   printf("\nEnter the radius of circle : 1");
   scanf("%d",&radius);
