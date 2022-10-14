3e8d8ccccb43cdc08f4cf9b6c055d4628b369194
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-14 14:07:52 +0530

    Update 27.updating contents of array.c

--- 27.updating contents of array.c
+++ 27.updating contents of array.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 main()
 {
+  printf("registration number = RA2211042010033");
   int stock[6][5], brand_code , watt_code , number ;
   while(scanf("%d %d %d", &brand_code , &watt_code, &number)
         ! = EOF)
