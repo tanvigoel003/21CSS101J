3afdb19a00574c50c259e3e1fc18286b8e7a0751
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-14 14:29:23 +0530

    Update 34.alternate method  to pick largest number.c

--- 34.alternate method  to pick largest number.c
+++ 34.alternate method  to pick largest number.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 main()
 {
+  printf("registration number = RA2211042010033"\n);
   int a, b, c, big ;
   scanf("%d %d %d , &a, &b, &c);
         printf("a = %4d, b = %4d, c = %4d\n" , a, b, c);
