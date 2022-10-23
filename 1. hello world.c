c2bd5c67c854657c1d270f5320a96122fff389c1
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-23 17:29:56 +0530

    Update 1. hello world.c

--- 1. hello world.c
+++ 1. hello world.c
@@ -1,7 +1,7 @@
 # include < studio.h>  //including header file in our program 
 int main()       // main() where the execution begins 
 {
-  printf("registration number = RA112211042010033",\n");
-printf("Hello World");
-return 0;
+  printf("registration number = RA112211042010033\n"); //printing registration number 
+printf("Hello World"); // printing hello world 
+return 0; 
 }
