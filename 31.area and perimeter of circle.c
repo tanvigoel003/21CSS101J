71c3284e33c2b7be15f2bf399e367e73d008f522
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-23 20:09:22 +0530

    Update 31.area and perimeter of circle.c

--- 31.area and perimeter of circle.c
+++ 31.area and perimeter of circle.c
@@ -2,10 +2,10 @@
 int main() 
 {
   printf("registration number = RA2211042010033"\n);
-  float radius , area;
-  printf("\nEnter the radius of circle : 1");
-  scanf("%d",&radius);
-  area = 3.14 * radius * radius;
-  printf("\nArea of circle : %f", area);
+  float radius , area; // float is used because the values can have foating points  
+  printf("Enter the radius of circle : ");
+  scanf("%d",&radius); // take the value of radius value from the user 
+  area = 3.14 * radius * radius; // define value of pi in begiening to write pi instead of 3.14 in the formula 
+  printf("Area of circle : %f", area);
   return 0;
 }
