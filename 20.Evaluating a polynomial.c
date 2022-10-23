355a00a9a3e49f12ec888edbc63d8dfdb35d01e7
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-23 19:57:22 +0530

    Update 20.Evaluating a polynomial.c

--- 20.Evaluating a polynomial.c
+++ 20.Evaluating a polynomial.c
@@ -4,7 +4,7 @@ main()
   printf("Registration number = RA22110420110033"\n);
          int i, n;
   float x , a[20] , polynomial;
-  scanf("%d %f , &n , &x);
+  scanf("%d %f" , &n , &x);
         for (i = 0; i <= n ; ++i)
         scanf("%f", &a[i]);
         polynomial = a[n];
