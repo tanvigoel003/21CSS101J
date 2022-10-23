393dd483087d4c352d2ee15946888b5617013048
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-23 18:54:11 +0530

    Update 19meters to feet.c

--- 19meters to feet.c
+++ 19meters to feet.c
@@ -2,11 +2,11 @@
 int main()
 {
   printf("Registration number = RA22110420110033"\n);
-  int meters;
-  float feet;
-  pritf("enter the value for meters = 50");
+  int meters; // value given by the user will be in integer 
+  float feet; // value after conversion can be a floating type 
+  pritf("enter the value for meters = ");
   scanf("%d" , &meters);
-  feet = meters * 3.28084; 
-  printf("%d meters are equal to %.2f feet" , meters , feet);
+  feet = meters * 3.28084; // formula for conversion 
+  printf("%d meters are equal to %.2f feet" , meters , feet); //.2f for the precision of the floating value to be printed 
   return 0;
 }
