dece0decbdc7ed8c491e95487c976c5237f688a2
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-23 20:01:47 +0530

    Update 25.finding area and perimeter of a rectangle.c

--- 25.finding area and perimeter of a rectangle.c
+++ 25.finding area and perimeter of a rectangle.c
@@ -3,11 +3,11 @@
 main()
 {
   printf("registration number = RA22110420110033"\n);
-int p , q , area , perimeter 
+int p , q , area , perimeter // let p , q be sides of the rectangle in cm 
 p = 4 ; 
 q = 6 ; 
-area = p*q ;
-perimeter = 2*(p+q);
+area = p*q ; // formula of area of a rectangle 
+perimeter = 2*(p+q); // formula of perimeter of a rectangle 
 printf(" area = %d \n" , area); 
 printf(" perimeter = %d \n" , perimeter ); 
 } /*End of main */ 
