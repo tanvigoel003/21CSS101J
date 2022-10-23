824ad7021620d6e4e6f48bb2786dfc536ebd1973
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-23 18:41:17 +0530

    Update 11.program for swapping two numbers.c

--- 11.program for swapping two numbers.c
+++ 11.program for swapping two numbers.c
@@ -3,8 +3,8 @@
 void main()
 {
   printf("Registration number = RA2211042010033"\n);
-  int x = 10 , y = 15 , temp;
-  temp = x;
+  int x = 10 , y = 15 , temp; // declaring the variables to be used 
+  temp = x; // renaming the variables to replace the values 
   x = y;
   y = temp;
   printf("X = %d and y =%d" , x , y );
