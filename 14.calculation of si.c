63190c6f4f370c95efda96e8a128e590f0209f68
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-23 18:46:00 +0530

    Update 14.calculation of si.c

--- 14.calculation of si.c
+++ 14.calculation of si.c
@@ -1,9 +1,9 @@
 /* Calculation of simple intrest */
 int main()
 {
-  printf("Registration number = RA22110420110033"\n):
-  int p, n ;
-  float r , si ; p= 1000;n = 3 ; r = 8.5 ; 
+  printf("Registration number = RA22110420110033"\n);
+  int p, n ; // int here means the values of p:- principal and n :- time will be an integer value 
+  float r , si ; p= 1000;n = 3 ; r = 8.5 ; // float is used as rate is in decimals , to print decimal values we use float 
   /* formula for simple intrest */
   si = p*n*r/100;
   printf ( " %f" , si ); 
