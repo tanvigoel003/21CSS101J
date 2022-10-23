45670e3191f471b60148b5eaaca679eb0ae1e196
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-10-23 20:06:30 +0530

    Update 30.division of two number.c

--- 30.division of two number.c
+++ 30.division of two number.c
@@ -2,12 +2,12 @@
 int main()
 {
   printf("registration number = RA2211042010033"\n);
-  int num1,num2,div;
-  printf("\tEnter Two Numbers\n");
+  int num1,num2,div; // declaring the variable 
+  printf("Enter Two Numbers\n");
   printf("-------- \n");
-  printf("Enter First Number : 40 ");
-  scanf("%d" , &num1);
-  printf("\nEnter Second Number : 7");
+  printf("Enter First Number :  ");
+  scanf("%d" , &num1); // read the input from the user 
+  printf("\nEnter Second Number : ");
   scanf("%d",&num2);
   div = num1/num2;
   printf("\nDivision of %d & %d is = %d" , num1, num2 ,div);
