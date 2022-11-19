479120d7fc6fb28aee17c4720538ca9a6b8a1c10
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-19 18:50:04 +0530

    Create 52.variable datatype.c

---
+++ 52.variable datatype.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+  int age = 18; /* here age is variable name and int is datatype which tells about the type of data stored in variable age , int is used to store integer values */
+  float pi = 3.14; // float data type is used to indicate decimal values are stored in the variable 
+  char underscore = '_'; // char is used to store a charecter , it is always writen in single quotes 
+  printf("registration number :- RA2211042010033");
+  return 0;
+}
+  
