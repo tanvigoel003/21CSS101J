8ad3153ab16b0269c7d9ac2348fa54366da397f4
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-19 20:32:09 +0530

    Update 20.Evaluating a polynomial.c

--- 20.Evaluating a polynomial.c
+++ 20.Evaluating a polynomial.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 main()
 {
-  printf("Registration number = RA22110420110033"\n);
-         int i, n;
+  printf("Registration number = RA22110420110033\n");
+  int i, n;
   float x , a[20] , polynomial;
   scanf("%d %f" , &n , &x);
-        for (i = 0; i <= n ; ++i)
-        scanf("%f", &a[i]);
-        polynomial = a[n];
-        for (i = n ; i >= 1; --i)
-        polynomial = a[n];
-        for (i = n; i >= 1; --i)
-        polynomial = a[i-1] + x * polynomial;
-        printf("x = %f, polynomial value = %f\n" , x, polynomial);
-        }
+  for (i = 0; i <= n ; ++i)
+  scanf("%f", &a[i]);
+  polynomial = a[n];
+  for (i = n ; i >= 1; --i)
+  polynomial = a[n];
+  for (i = n; i >= 1; --i)
+  polynomial = a[i-1] + x * polynomial;
+  printf("x = %f, polynomial value = %f\n" , x, polynomial);
+}
