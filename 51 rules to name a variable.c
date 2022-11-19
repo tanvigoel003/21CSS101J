cf768829fec964146c96ddc03f512739fcf0b8cd
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-19 18:34:31 +0530

    Create 51 rules to name a variable.c

---
+++ 51 rules to name a variable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+  printf("registration number :- RA2211042010033\n");
+  char name = Tanvi; //variables are case sensitive 
+  char NAME = tanvi; 
+  /* variables are case sensitive so both nvalues will be stored in different memory location */
+  int final_price = 100;  // only underscore is allowed in variable naming 
+  int age = 20; // naming the variable accpording to its value helps while wriing huge codes 
+  
+  
+ return 0;
+}
