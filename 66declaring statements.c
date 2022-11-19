da3c1ceb1d9ac0a30d936f700c6bf9e175ec6074
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-20 00:09:16 +0530

    Create 66declaring statements.c

---
+++ 66declaring statements.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(void)
+{
+  printf("registration number : RA2211042010033\n");
+  int a =22;
+  int b = a + 8;
+  int c = b *3 , d;
+   float x,y,z;
+  x = y = z = 5.6;
+}
