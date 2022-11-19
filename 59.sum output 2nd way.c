9a4324cdb7ef7846938ca8da257b8621d2099a5f
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-19 22:58:26 +0530

    Create 59.sum output 2nd way.c

---
+++ 59.sum output 2nd way.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main(void)
+{
+int number1 , number2;
+printf("registration number : RA2211042010033\");
+printf(" enter first number : ");
+scanf("%d",&number1);
+printf(" enter second number : ");
+scanf("%d" ,&number2);
+printf("sum of both numbers = %d",number1 + number2);
+}
+
