0114496fc794eaf0ef95c6c20163639ff44ff64c
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-19 19:59:54 +0530

    Create 55output.c

---
+++ 55output.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+printf("this function is used to print everything writen in double quotes");
+/* the output on the screen will be
+    this function is used to print everything writen in double quotes */ 
+    
+    printf("here we are using new line function.\n this line will be printed in the next line .");
+    /* the output on the screen will be as 
+        here we are using new line functon. 
+        this line will be printed in the next line .*/
+        printf("registration number : RA2211042010033");
+        return 0;
+        }
