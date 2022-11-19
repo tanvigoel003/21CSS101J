6247b24ef420305eaff41c859c02d609de943506
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-19 23:26:27 +0530

    Create 61area of a parallelogram.c

---
+++ 61area of a parallelogram.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main(void)
+{
+  printf("registration number : RA2211042010033\n");
+  int base , height; 
+  printf("enter value of base : ",base); 
+  scanf("%d",&base);
+  printf("enter value of height : ",height);
+  scanf ("%d",&height);
+  int area = base * height;
+  printf("area of the parallelogram is : %d",area);
+}
