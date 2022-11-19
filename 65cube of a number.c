2bb0b2b21e04d02bcded486b9a47c96ae7248730
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-19 23:58:14 +0530

    Create 65cube of a number.c

---
+++ 65cube of a number.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main(void)
+{
+  printf("registration number : RA2211042010033");
+  float number;
+  printf ("enter the number : " );
+  scanf("%f",&number);
+  printf("cube of the number is %f", number * number * number );
+}
