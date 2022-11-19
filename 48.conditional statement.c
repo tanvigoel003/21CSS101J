9c8b07ad0c40532e6b1851978e3e3c78b211d126
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-19 22:20:50 +0530

    Update 48.conditional statement.c

--- 48.conditional statement.c
+++ 48.conditional statement.c
@@ -2,7 +2,8 @@
 void main()
 {
   int int1, int2;
-  int1 = 15, int2 = 15;    
+  int1 = 15, int2 = 15; 
+  printf("registration number = RA2211042010033\n");
   printf("Input the values for Number1 and Number2 : ");
   scanf("%d %d", &int1, &int2);
   if (int1 == int2) {printf("Number1 and Number2 are equal\n");}
