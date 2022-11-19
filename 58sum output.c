b1900954dfcc565e4c3921f602d38ea378689a0b
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-19 22:53:14 +0530

    Create 58sum output.c

---
+++ 58sum output.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main(void)
+{
+  printf("registration number = RA2211042010033\n");
+  int number1 , number2 ; 
+  printf(" enter  first number : ");
+  scanf("%d",&number1);
+  printf("enter second number : ");
+  scanf("%d",&number2);
+  int sum = number1 + number2;
+  printf("sum of both  number is : %d\n",sum);
+}
