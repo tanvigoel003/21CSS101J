5d64d35484e4d546dd6fd7fa3f7f97583b06556d
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-19 21:55:48 +0530

    Update 39 conversion.c

--- 39 conversion.c
+++ 39 conversion.c
@@ -2,15 +2,13 @@
 float kmph;              /* kilometers per hour */
 float miph;              /* miles per hour (to be computed) */
 char  line_text[50];      /* a line from the keyboard */
-
 int main()
 {
-	printf("Input kilometers per hour: 15");
-	fgets(line_text, sizeof(line_text), stdin);
-	sscanf(line_text, "%f", &kmph);
-
-	miph = (kmph * 0.6213712);
-	printf("%f miles per hour\n", miph);
-
-	return(0);
+ printf("registration number : RA2211042010033\n");
+ printf("Input kilometers per hour: 15");
+ fgets(line_text, sizeof(line_text), stdin);
+ sscanf(line_text, "%f", &kmph);
+ miph = (kmph * 0.6213712);
+ printf("%f miles per hour\n", miph);
+ return(0);
 }
