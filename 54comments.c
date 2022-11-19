4355782fe6726d83288c414f015c25c7c88d3090
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-19 19:49:56 +0530

    Create 54comments.c

---
+++ 54comments.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+printf("comments are not a part of code . It is written to help other programmer to understand the code in a better way . There are two types of comments single line comment and multiline comment.");
+// this is a single line comment
+/*this is a multiline comment*/
+printf("registration number ; RA2211042010033");
+return 0;
+}
