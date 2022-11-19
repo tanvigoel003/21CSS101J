5ac3ccf98a61150d860cd5942020151c564b5761
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-19 21:08:37 +0530

    Update 38.volume of sphere.c

--- 38.volume of sphere.c
+++ 38.volume of sphere.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-float myradius;          /* radius of the sphere */
-float myvolume;          /* volume of the sphere (to be computed) */
-char line_text[50];      /* a line from the keyboard */
+float radius;          /* radius of the sphere */
+float volume;          /* volume of the sphere (to be computed) */
+char line_text[50];      
 
-/* the value of pi to 50 places, from wikipedia */
+/* the value of pi to 50 places */
 const float PI = 3.14159265358979323846264338327950288419716939937510;
-
-int main() {
-	printf("Input the radius of the sphere : ");
-	fgets(line_text, sizeof(line_text), stdin);
-	sscanf(line_text, "%f", &myradius);
-
-	myvolume = (4.0 / 3.0) * PI * (myradius * myradius * myradius);  /* volumn=(4/3) * pi * r^3*/
-	printf("The volume of sphere is %f.\n", myvolume);
-
-	return(0);
+int main()
+{
+printf("registration number : RA2211042010033\n");
+printf("Input the radius of the sphere : ");
+fgets(line_text, sizeof(line_text), stdin);
+sscanf(line_text, "%f", &radius);
+volume = (4.0 / 3.0) * PI * (radius * radius * radius);  /* volumn=(4/3) * pi * r^3*/
+printf("The volume of sphere is %f.\n", volume);
+return(0);
 }
