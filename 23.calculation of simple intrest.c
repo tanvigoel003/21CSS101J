b470122aa3487520c018fdf107b6bcef533bdb66
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-19 20:34:32 +0530

    Update 23.calculation of simple intrest.c

--- 23.calculation of simple intrest.c
+++ 23.calculation of simple intrest.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 main()
 {
-  printf("registration number = RA22110420110033"\n);
+  printf("registration number = RA22110420110033\n");
 int p, n; 
 float r , si; 
 p= 1000;
@@ -10,3 +10,4 @@ r = 8.5;
 /* formula for simple intrest */ 
 si = p*n*r/100; 
 printf ( " %f" , si ); 
+}
