4a387e186f3915fbbf1b1c68c47dd2b89ad420eb
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-19 23:11:18 +0530

    Create 60area of square.c

---
+++ 60area of square.c
@@ -0,0 +1,9 @@
+#include<stdio.h> 
+void main(void)
+{
+// area of square when side is given , using area as variable
+printf("registration number : RA2211042010033\n");
+int side = 5;
+int area = side * side ; 
+printf("area of the square is : %d",area);
+}
