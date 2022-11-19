d7f16ae82db25b0252dd685af11bb5b3768ea78f
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-19 22:05:09 +0530

    Update 40hour to min.c

--- 40hour to min.c
+++ 40hour to min.c
@@ -1,25 +1,19 @@
 #include <stdio.h>
-
 int hrs;          /* given number of hours */
 int mins;         /* given number of minutes */
 int tot_mins;     /* total number of minutes (to be computed) */
-
 const int MINaHOUR = 60;      /* number of minutes in an hour */
-
 char line_text[50];      /* line of input from keyboard */
-
-int main() {
-	printf("Input hours: 5");
-	fgets(line_text, sizeof(line_text), stdin);
-	sscanf(line_text, "%d", &hrs);
-
-	printf("Input minutes: 37");
-	fgets(line_text, sizeof(line_text), stdin);
-	sscanf(line_text, "%d", &mins);
-
-	tot_mins = mins + (hrs * MINaHOUR);
-
-	printf("Total: %d minutes.\n", tot_mins);
-
-	return(0);
+int main() 
+{
+printf("registration number : RA2211042010033\n");
+printf("Input hours: 5");
+fgets(line_text, sizeof(line_text), stdin);
+sscanf(line_text, "%d", &hrs);
+printf("Input minutes: 37");
+fgets(line_text, sizeof(line_text), stdin);
+sscanf(line_text, "%d", &mins);
+tot_mins = mins + (hrs * MINaHOUR);
+printf("Total: %d minutes.\n", tot_mins);
+return(0);
 }
