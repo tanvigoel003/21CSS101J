c32933a47798775ec7f5b0df20e6807e4fd22f90
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-19 20:23:47 +0530

    Update 15. sum of series.c

--- 15. sum of series.c
+++ 15. sum of series.c
@@ -8,4 +8,5 @@ int main()
   printf("x =%f \n" , x) ; x2 = x*x ;x4 = x2*x2 / 24.0 ; x6 = x4*x2 / 30.0 ;x8 = x6*x2 / 56.0 ;x10 = x8*x2 / 90.0 ;
   sum = 1.0-0.5*x2 + x4 - x6 + x8 - x10;
   printf(" value of x = %f , sum =%f\n" , x , sum );
+  return 0;
 } /* End of main */
