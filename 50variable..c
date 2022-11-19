e38a827ece95111f258c0e12c3f7bcefd9bbe297
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-19 18:27:30 +0530

    Create 50variable..c

---
+++ 50variable..c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+  printf("registration number :- RA2211042010033\n");
+  int number = 25; //here number is a variable storing the value 25
+  float pi = 3.14; // here pi varaiable storing the value of pi 
+  char star = '*' //here star stores the sign of star  
+return 0;
+}
