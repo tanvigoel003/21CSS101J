39b2d73163570a3e77a94171bcf0e986593924d2
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-19 21:02:15 +0530

    Update 36.block of f.c

--- 36.block of f.c
+++ 36.block of f.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 int main()
 {
+  printf("registration number : RA2211042010033\n");
   printf("######\n");
   printf("#\n");
   printf("#\n");
   printf("#####\n");
-    printf("#\n");
+  printf("#\n");
   printf("#\n");
   printf("#\n");
   return(0);
