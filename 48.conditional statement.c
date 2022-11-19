e01b20ee286c45a980fbfc3e39bda0fc5f4c90fe
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-19 22:19:37 +0530

    Update 48.conditional statement.c

--- 48.conditional statement.c
+++ 48.conditional statement.c
@@ -1,13 +1,10 @@
-include <stdio.h>
+#include <stdio.h>
 void main()
 {
-    int int1, int2;
-    int1 = 15, int2 = 15;
- 
-    printf("Input the values for Number1 and Number2 : ");
-    scanf("%d %d", &int1, &int2);
-    if (int1 == int2)
-        printf("Number1 and Number2 are equal\n");
-    else
-        printf("Number1 and Number2 are not equal\n");
+  int int1, int2;
+  int1 = 15, int2 = 15;    
+  printf("Input the values for Number1 and Number2 : ");
+  scanf("%d %d", &int1, &int2);
+  if (int1 == int2) {printf("Number1 and Number2 are equal\n");}
+  else {printf("Number1 and Number2 are not equal\n");}
 }
