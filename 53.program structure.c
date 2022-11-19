7386ebd479082ba20740395e5aa15f113771c9ed
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-19 18:58:40 +0530

    Create 53.program structure.c

---
+++ 53.program structure.c
@@ -0,0 +1,7 @@
+#include <stdio.h>  /* this is a header file without the header file the code won't run , so it's necessarry to write it in the begining of code */
+int main()          //this is a function and the compiler starts compiling the code from here 
+{                     // code block we arite the functions to be performed within this code block 
+printf("REGISTRATION NUMBER :- RA2211042010033");
+/* whatever we write in printf function gets printed on the screen */ 
+return 0;             // ; it is like a full stop of c language . this shows the end of code 
+}                     // code block ends 
