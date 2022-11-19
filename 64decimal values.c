0ab0f3c7c4f1be08e313db1840727f1e44f51c9d
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-19 23:40:08 +0530

    Update 64decimal values.c

--- 64decimal values.c
+++ 64decimal values.c
@@ -1,1 +1,13 @@
-
+include <stdio.h>
+void main(void)
+{
+  printf("registration number : RA2211042010033\n");
+  float length , breadth , height;
+  printf("enter length of cuboid : ");
+  scanf("%f",&length);
+  printf("enter breadth of cuboid : ");
+  scanf("%f" ,&breadth);
+  printf("enter height of cuboid : "); 
+  scanf("%f",&height);
+  printf("volume of cuboid is %f" , length * breadth * height);
+}
