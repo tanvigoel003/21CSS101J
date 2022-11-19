a1d0190f9308be680980c2291c904e90c499b817
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-19 22:45:20 +0530

    Create 57input scanf.c

---
+++ 57input scanf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+int age;
+printf("registration number = RA2211042010033\n");
+printf("enter age : ");
+scanf("%d",&age); // it is used to read value from user 
+printf("age is : %d",age);
+return 0;
+}
