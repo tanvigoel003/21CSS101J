e5191dbe9d4f113f43c3b2a8473a412de4734571
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-19 20:18:28 +0530

    Create 56printing various types of  output.c

---
+++ 56printing various types of  output.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+  int age = 20;
+  float pi = 3.14;
+  char star = '*';
+  printf("registration number : RA2211042010033");
+  // case 1 to print an integer 
+  printf(" age is %d",age);
+  /* output will be displayed as age is 20
+     %d is used to print an integer value in print */
+  // case 2 to print a decimal value 
+  printf("The value of pi is %f",pi);
+  /* the output will be printed as 
+  The value of pi is 3.14 
+  %f is used to print floating or decimal values */
+  // case 3 to print a charecter 
+  printf("The symbol of star is %c",star);
+  /* the output will be printed as 
+  The symbol of star is * */
+  return 0; 
+}
