5954ef647e6389a9888f9990863ff9d50945a404
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-19 20:28:59 +0530

    Update 2. printf.c

--- 2. printf.c
+++ 2. printf.c
@@ -1,7 +1,8 @@
 #include<studio.h>
 int main()
 {
-  printf("registration number = RA2211042010033"\n);
+  printf("registration number = RA2211042010033\n");
   int num1 , num2; // declaring two numbers 
   printf("num1 = 2, num2 = 5\n"); // printing the two declare number with its value 
   return 0;
+}
