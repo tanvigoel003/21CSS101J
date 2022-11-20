157b096fcb96091cc627cfbc2c0e84a4edd4a0d8
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-20 16:32:44 +0530

    Create 74 logical operator not.c

---
+++ 74 logical operator not.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main(void)
+{
+printf("registration number : RA2211042010033\n");
+printf("%d", (4==4));                             // this will print the output as 1
+printf("%d", !(4==4));                            // this will print the output as 0 as not changes the value from true to false and vice- versa
+}
