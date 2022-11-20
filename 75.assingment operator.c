83416a18b16d8cebd19fbbfd04743a1187c9cbe8
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-20 16:44:05 +0530

    Create 75.assingment operator.c

---
+++ 75.assingment operator.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main(void)
+{
+  printf("registration number : RA2211042010033\n");
+  int a = 4;
+  int b = 1;
+  int a = a + b;
+  printf("%d",a);   // traditional way to write 
+ 
+  
+  int d = 4;
+  int e = 1;
+  int d += e;  // d = d + e , short hand way to write the same 
+  printf("%d\n", d);
+}
