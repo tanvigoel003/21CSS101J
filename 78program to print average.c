fd945174fd24932516f7b5553e66aa27b2c42339
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-21 00:28:00 +0530

    Create 78program to print average.c

---
+++ 78program to print average.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(void)
+{
+  printf("registration number :RA2211042010033\n");
+  int a, b, c;  //a , b, c are 3 numbers 
+  printf("enter the numbers : ");
+  scanf("%d %d %d",&a, &b, &c);
+  printf("average of 3 numbers are : %d",(a + b + c ) / 3);
+}
