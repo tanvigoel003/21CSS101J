6ef7afbc7a464f97d94560b4f609e318886db7ae
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-20 15:11:49 +0530

    Create 71not equal to operator.c

---
+++ 71not equal to operator.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main(void)
+{
+  printf("registration number : RA2211042010033\n");
+  printf("%d", 4 == 4); // the output will be 1 
+  printf("%d", 4 != 4); //the output will peint 0 as 4 !=4 (read as 4 is not equal to 4) is a false statement 
+}
