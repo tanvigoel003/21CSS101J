e200ee8bd663ff81383dbce3e70235db3ded84e2
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-20 14:59:42 +0530

    Create 70comparitive operator.c

---
+++ 70comparitive operator.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(void)
+{
+  printf("registration number : RA2211042010033\n");
+  printf("%d\n", 3>3);    //this will print 0 as output because 3>3 is false 
+  printf("%d\n", 3>=3);   //this will print 1 as output because 3>=3 is true
+  printf("%d\n", 3>4);    //this will print 0 as output because 3>4 is false
+  printf("%d\n", 4>3);    // this will print 1 as output because 4>3 is truev
+}
