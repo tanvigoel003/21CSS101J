e800c0196d5c20ce61fbec0779031d934f3b13d4
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-20 18:12:59 +0530

    Create 77 to check even or odd.c

---
+++ 77 to check even or odd.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main(void)
+{
+  printf("registration number : RA2211042010033\n");
+  int number;
+  printf("enter the number : ");
+  scanf("%d",&number);
+  if (number % 2 == 0 ) {printf("The number is divisible by 2.");}
+  else {printf(" The number is not divisible by 2 ");}
+}
+                    
