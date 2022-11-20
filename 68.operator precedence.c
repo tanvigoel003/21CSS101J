d5ea42ef054e9c959fd90a4d8e9f6bc672976a3d
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-20 10:45:21 +0530

    Create 68.operator precedence.c

---
+++ 68.operator precedence.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main(void)
+{
+  printf("registration number :RA2211042010033\n");
+  printf(" 5 x 2 / 2 x 3 = %d", 5 * 2 / 2 * 3); 
+  /* multiplication and division have same oder of precedence so we use the assosiative law 
+     assosiative law is followed from left to right so 5 and two will be multiplied the resultant will be divided by two and the new resultant will be multiplied by 3 
+     the output printed will be 5 x 2 / 2 x 3 = 15  */
+}
