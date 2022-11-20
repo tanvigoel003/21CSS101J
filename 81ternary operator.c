e310cce29c6f1bec09414ec3e222c7a5ee5ad8b2
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-21 01:15:33 +0530

    Create 81ternary operator.c

---
+++ 81ternary operator.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(void)
+{
+  printf("registration number : RA2211042010033\n");
+  int age;
+  printf("enter age : ");
+  scanf("%d", &age);
+  age < 18 ? printf("the person is not adult\n") : printf("the person is adult\n");
+}
+  
