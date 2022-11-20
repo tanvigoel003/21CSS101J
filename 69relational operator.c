e506540797a1941c68e473ac5d599882029d886a
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-20 14:16:38 +0530

    Create 69relational operator.c

---
+++ 69relational operator.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main(void)
+{
+  printf("registration number : RA2211042010033\n");
+  printf(" %d \n",4 == 4);  // this will print 1 as output which means 4==4 is true.
+  printf("%d\n",5==4);     // this will print 0 as output which means 5==4 is false .
+}
