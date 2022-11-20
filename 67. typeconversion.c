ef72554ff338d6d8ad997dc6baa41507004dae5b
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-20 10:01:33 +0530

    Create 67. typeconversion.c

---
+++ 67. typeconversion.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main(void)
+{
+printf("registration number = RA2211042010033\n");
+printf("mathematical operations on two integer type number gives integer nuber as output . eg : 2 x 2 = %d", 2*2);
+printf("mathematical operation on two float type number gives float number as output . EG : 8.35 - 5.25 = %d", 8.35 - 5.25);
+printf("mathematical operation on one integer and one float type number gives float number in output . Eg : 20 + 80.00 = %d", 20 + 80.00);
+}
