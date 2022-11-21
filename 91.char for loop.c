717de880633dacba4384d671b32bd393252343db
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-21 21:20:55 +0530

    Create 91.char for loop.c

---
+++ 91.char for loop.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(void)
+{
+  printf("registration number : RA2211042010033\n");
+  printf("english alphabets are : \n");
+  char ch;
+  for (ch = 'A'; ch <=' Z' ; ch ++)
+  {printf("%c",ch);}
+}
