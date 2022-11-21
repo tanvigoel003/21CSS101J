6bca1b1896dc747cd95540d795a59f1d8e02f348
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-22 01:51:54 +0530

    Create 106making a function.c

---
+++ 106making a function.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void printhello(void);
+void main(void)
+{
+  printhello();
+  printf("registration number : RA2211042010033\n");
+}
+void printhello(void)
+{
+  printf("hello world\n");
+}
+         
