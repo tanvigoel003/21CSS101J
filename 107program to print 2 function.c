1352b1c3b85eefac03f9dffb2585635f21f9c289
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-22 02:04:34 +0530

    Create 107program to print 2 function.c

---
+++ 107program to print 2 function.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void printhello(void);
+void printgoodbye(void);
+  void main(void)
+{
+  printf("registration number: RA2211042010033\n");
+  printhello();
+  printgoodbye();
+}
+
+ void printhello(void)
+{  printf("hello\n");}
+
+
+ void printgoodbye(void)
+{  printf("goodbye\n");}
