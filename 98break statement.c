0adeb53249e32a860319d0a4d31fa232abbc16e8
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-22 00:20:59 +0530

    Create 98break statement.c

---
+++ 98break statement.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(void)
+{
+  printf("registration number : RA2211042010033\n");
+  for(int i = 1; i <= 10; i ++)
+  {
+    if (i == 5)
+    {
+      break;
+    }
+    printf("%d\n",i);
+  }
+  printf("end\n");
+}
