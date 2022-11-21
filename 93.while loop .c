5574f3a01cd0cadf787f1fe7ada0675bb134af17
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-21 21:47:22 +0530

    Create 93.while loop .c

---
+++ 93.while loop .c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(void)
+{
+  printf("registration number : RA2211042010033\n");
+  printf("printing numbers from 11 to 20 :- \n");
+  int number = 11;
+    while(number <= 20){printf("%d",number);
+      number ++}
+  }
