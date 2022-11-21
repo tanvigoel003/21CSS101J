f574a4b8dff99e2bef673e72809b547ab22f5483
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-21 20:05:42 +0530

    Create 88printing number using for loop.c

---
+++ 88printing number using for loop.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main(void)
+{
+  printf("registration number : RA2211042010033\n");
+  printf("number in descending oder is :-\n");
+  for ( int i = 10; i >= 0; i --) {
+      printf("%d\n",i);
+  }
+}
