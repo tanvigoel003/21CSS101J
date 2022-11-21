073f6bdfa32323e1c8ffe3d016621d764cdb4f60
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-21 20:31:07 +0530

    Create 89increment operator.c

---
+++ 89increment operator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(void)
+{
+  printf("registration number : RA2211042010033\n");
+  int i;
+  printf(" enter number to be incremented : \n");
+  scanf("%d",&i);
+  printf("increment operator used , value will be same as %d because it will first put the value and increment in the next step.\n",i++);                      
+  printf("after increment operator used in the previous line the value is now incremented %d\n",i);                                         
+  
+}
