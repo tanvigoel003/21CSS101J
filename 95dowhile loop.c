b84fcb67c984d448d2aae8423ddb6351cbe3a3b8
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-21 22:27:43 +0530

    Create 95dowhile loop.c

---
+++ 95dowhile loop.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(void)
+{
+  printf("registration number : RA2211042010033\n");
+  printf("printing numbers from 0 to 9\n");
+  int i; 
+  i = 0; 
+  do {printf("%d",i); i++;} while (i <10);
+}
