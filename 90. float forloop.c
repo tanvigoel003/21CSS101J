996d26c2067dd5bcb2f275a109faa8041597a834
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-21 20:41:45 +0530

    Create 90. float forloop.c

---
+++ 90. float forloop.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(void)
+{
+  printf("registration number : RA2211042010033\n");
+  float number;
+  printf("decimal values betwwen 1.0 and 10.0are :-\n");
+  for (number = 2.0; number < 10.0; number ++)
+  {printf("%f\n",number);}
+}
