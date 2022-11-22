3f0f01de8c707e82c5e3dc69becfaa4af85a488e
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-22 17:46:44 +0530

    Create 110program to convert upper case to lowercase.c

---
+++ 110program to convert upper case to lowercase.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main(void)
+{
+  printf("registration number :- RA2211042010033\n");
+  char ch;
+  printf("upper case charecter : ",ch);
+  scanf("%c", &ch);
+  ch = ch +32;
+  printf("lowercase : %c\n",ch);
+}
