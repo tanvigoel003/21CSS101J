c3feabb676ce2ac8eb25f41562fdcad6249576ad
tanvigoel003 <113626515+tanvigoel003@users.noreply.github.com>
2022-11-22 18:15:23 +0530

    Create 111comparision.c

---
+++ 111comparision.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main(void)
+{
+int num1, num2, num3; 
+printf("Enter 3 integers : ",num1, num2, num3);
+  scanf("%d %d %d",&num1, &num2, &num3);
+  if (num1 > num2 && num1 > num3)
+  {
+    printf("%d is largest\n",num1);
+  }
+  else if (num2 > num3 && num2 > num3)
+  {
+    printf("%d is largest\n",num2);
+  }
+  else 
+  {
+    printf("%d is largest\n",num3);
+  }
+  if (num1 < num2 && num1 < num3)
+  {
+    printf("%d is smallest\n",num1);
+  }
+  else if (num2 < num3 && num2 < num3)
+  {
+    printf("%d is smallest\n",num2);
+  }
+  else 
+  {
+    printf("%d is smallest\n",num3);
+  }
+}
